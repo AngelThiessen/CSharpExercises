740e9717f83fea7b94ae261de3bfbf33ca7a7223
AngelThiessen <111193551+AngelThiessen@users.noreply.github.com>
2022-08-13 14:37:54 -0400

    Add files via upload

---
+++ elementaryOperations1/Program.cs
@@ -0,0 +1,64 @@
+﻿
+namespace elementaryOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // lines 9-12, you're entering the numbers for the operations
+            Console.WriteLine("Hello, please enter the first number: ");
+            double firstNum = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Thank you. Please enter the next number: ");
+            double secondNum = Convert.ToDouble(Console.ReadLine());
+
+            //addition
+            //double addition = firstNum + secondNum;
+
+            //substraction
+            //double subtraction = firstNum - secondNum;
+
+            //multiplication
+            //double multiply = firstNum * secondNum;
+
+            //division exception
+            //if ()
+
+            //division exception
+            //double divide = firstNum / secondNum;
+
+            Addition (firstNum, secondNum);
+            Subtraction(firstNum, secondNum);
+            Multiplication(firstNum, secondNum);
+            Division(firstNum, secondNum);
+        }
+
+        static void Addition(double firstNum, double secondNum)
+        {
+            double sum = firstNum + secondNum;
+            Console.WriteLine("The sum is: " + sum);
+        }
+        static void Subtraction(double firstNum, double secondNum)
+        {
+            double difference = firstNum - secondNum;
+            Console.WriteLine("The difference is: " + difference);
+        }
+        static void Multiplication(double firstNum, double secondNum)
+        {
+            double product = firstNum * secondNum;
+            Console.WriteLine("The product is: " + product);
+        }
+        
+        static void Division(double firstNum, double secondNum)
+        {
+            if (secondNum == 0)
+            {
+                Console.WriteLine("Sorry, but you can't divide by zero");
+            }
+            else 
+            {
+                double quotient = firstNum / secondNum;
+                Console.WriteLine("The quotient is: " + quotient);
+            }
+        }
+    }
+}
---
+++ fahrenheitConversionCalculator/FahrenheitConversionCalculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace csPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, what number would you like to convert to Fahrenheit today?");
+            CtoF();
+        }
+    }
+    static void CtoF(double C)
+        {
+            double F = (C * 1.8) + 32;
+            if (F < -271.15)
+            {
+                Console.WriteLine("Temperature below absolute zero!");
+            }
+            else
+            {
+                Console.WriteLine(F);
+            }
+            C = Double.Parse(Console.ReadLine());
+        }
+}
